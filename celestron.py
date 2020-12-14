bc7945dd88a5a1ae31f7fba8a8d91e0c894398bc
Patricio Latini <p_latini@hotmail.com>
2020-12-13 23:23:08 -0300

    Added GPS Emulator

--- celestron.py
+++ celestron.py
@@ -7,9 +7,12 @@ import binascii
 import serial
 import keyboard
 import argparse
+from datetime import datetime,timezone
 
 global msgqueue
 msgqueue = ''
+global emulategps
+emulategps = False
 
 preamble = 0x3b
 
@@ -24,7 +27,7 @@ devices = { 0x01 : 'Main Board / Interconnection',
             0xb0 : 'GPS',
             0xb3 : 'Skyportal Accessory',
             0xb4 : 'Starsense Camera',
-            0xb5 : 'Nexstar EVO WiFi',
+            0xb5 : 'Nextstar EVO WiFi',
             0xb6 : 'Battery Power Controller',
             0xb7 : 'Charge Port',
             0xbf : 'Mount Lights'}
@@ -142,9 +145,35 @@ def decodemsg (msg):
           commandtext = commands[(device,command)]
       else:
           commandtext = 'UNKNOWN'
-
       output = devices[sender] + " (" + str(hex(sender)) + ") " + "-> " + devices[receiver] + " (" + str(hex(receiver)) + ") " + "--- " + commandtext + " (" + str(hex(command)) + ") " + "--- " + str(commandvalue)
       print (output)
+      if emulategps:
+        global gpslat,gpslon    
+        if str(hex(receiver)) == '0xb0':
+          if str(hex(command)) == '0x36':
+            transmitmsg(receiver,sender,command,'01')
+          if str(hex(command)) == '0xfe':
+            transmitmsg(receiver,sender,command,'0b01')
+          if str(hex(command)) == '0x33':
+            transmitmsg(receiver,sender,command,format(datetime.now(timezone.utc).hour, '02x')+format(datetime.now(timezone.utc).minute, '02x')+format(datetime.now(timezone.utc).second, '02x'))
+          if str(hex(command)) == '0x3':
+            transmitmsg(receiver,sender,command,format(datetime.now(timezone.utc).month, '02x')+format(datetime.now(timezone.utc).day, '02x'))
+          if str(hex(command)) == '0x4':
+            transmitmsg(receiver,sender,command,format(datetime.now(timezone.utc).year, '04x'))
+          if str(hex(command)) == '0x37':
+            transmitmsg(receiver,sender,command,'01')
+          if str(hex(command)) == '0x2':
+            if gpslon>=0:
+                gpslonhex=format(int(gpslon/360*pow(2,24)),'06x')
+            else: 
+                gpslonhex=format(int((gpslon+360)/360*pow(2,24)),'06x')
+            transmitmsg(receiver,sender,command,gpslonhex)
+          if str(hex(command)) == '0x1':
+            if gpslat>=0:
+                gpslathex=format(int(gpslat/360*pow(2,24)),'06x')
+            else: 
+                gpslathex=format(int((gpslat+360)/360*pow(2,24)),'06x')
+            transmitmsg(receiver,sender,command,gpslathex)
     else:
       print (hex(sender)," -> ", hex(receiver), "---", hex(command), "---", commandvalue, "CRC FAIL!")
 
@@ -162,17 +191,28 @@ def processmsgqueue():
     else:
       emptyqueue=1
 
-def sendmsg(receiver,command):
+def sendmsg(sender,receiver,command,value):
   global preamble
-  if connmode=='wifi' or connmode=='hc' :
-      sender = 0x20
-  if connmode=='serial':
-      sender = 0x04
-  length = 3
-  sum = length + sender + receiver + command
-  sum=65536-sum
-  sum=sum&255
-  output = "{:02x}{:02x}{:02x}{:02x}{:02x}{:02x}".format(preamble,length,sender,receiver,command,sum)
+  commandvalue=[]
+  byte=0
+  if sender=='':
+    if connmode=='wifi' or connmode=='hc' :
+        sender = 0x20
+    if connmode=='serial':
+        sender = 0x04      
+  for c in range(0,len(value),2):
+      byte = int(c/2+1)
+      value2 = int(value[c:c+2],16)
+      commandvalue.append(hex(value2))
+  length = 3 + int(len(value)/2)
+  valuesum = sum(int(i,16) for i in commandvalue)
+  summa = length + sender + receiver + command + valuesum
+  summa=65536-summa
+  summa=summa&255
+  output1 = "{:02x}{:02x}{:02x}{:02x}{:02x}".format(preamble,length,sender,receiver,command)
+  output2 = value
+  output3 = "{:02x}".format(summa)
+  output = output1 + output2 + output3
   decodemsg(output)
   hexoutput = binascii.unhexlify(output)
   return hexoutput
@@ -183,7 +223,8 @@ def scanauxbus(interval):
   print ("Initiating AUXBUS SCAN ")
   print ("-----------------------")
   for device in devices:
-    transmitmsg(device,0xfe)
+    transmitmsg('',device,0xfe,'')
+  time.sleep(3)
   print ("-----------------------")
   print (" Finished AUXBUS SCAN  ")
   print ("-----------------------")
@@ -210,14 +251,14 @@ def printhelpmenu():
   print ("d) Show Device List    ")
   print ("c) Send Command to Device")
   print ("k) Toggle Keepalive Send")
-  print ("x) Disable Keyboard Input")
+  print ("g) Toggle GPS Emulator")
   print ("h) Print this menu     ")
   print ("q) Quit                ")
   print ("-----------------------")
 
 
-def transmitmsg(receiver,command):
-    data = sendmsg(receiver,command)
+def transmitmsg(sender,receiver,command,value):
+    data = sendmsg(sender,receiver,command,value)
     if connmode == 'wifi':
         sock.send(data)
     if connmode == 'serial':
@@ -237,7 +278,7 @@ def transmitmsg(receiver,command):
 def keep_alive(interval):
     while True:
         if keepalive:
-            transmitmsg(0x10,0xfe)
+            transmitmsg('',0x10,0xfe,'')
         time.sleep(interval)
 
 def receivedata():
@@ -328,9 +369,11 @@ def launchthreads():
 
 
 def execute_code(connmodearg, port):
+  global emulategps
   global connmode
   global SERVER_IP, COM_PORT
   global keepalive
+  global gpslat,gpslon
 
   connmode = connmodearg
 
@@ -343,15 +386,16 @@ def execute_code(connmodearg, port):
   launchthreads()
 
   while True:
-    if keyboard.read_key() == "d":
+    inputkey = input("Enter Command:")
+    if inputkey == "d":
         printactivedevices()
 
-    if keyboard.read_key() == "c":
+    if inputkey == "c":
         printactivedevices()
         time.sleep(0.2)
         print ("-----------------------")
         print ("Choose device")
-        key1 = keyboard.read_key()
+        key1 = input("Enter Device:")
         filtercommands=[(k[1], v) for k, v in commands.items() if k[0]==int(activedevices[int(key1)],16)]
         for command in filtercommands:
             output = chr(97+filtercommands.index(command)) + ") " + str(hex(command[0])) + " (" + str(command[1]) + ") "
@@ -359,20 +403,10 @@ def execute_code(connmodearg, port):
         time.sleep(0.2)
         print ("-----------------------")
         print ("Choose command")
-        key2 = keyboard.read_key()
-        transmitmsg(int(activedevices[int(key1)],16),filtercommands[ord(key2)-97][0])
+        key2 = input("Enter Command:")
+        transmitmsg('',int(activedevices[int(key1)],16),filtercommands[ord(key2)-97][0],'')
 
-    if keyboard.read_key() == "x":
-        print ("-----------------------")
-        print ("   Key Input Disable   ")
-        print ("  Press y to Reenable  ")
-        print ("-----------------------")
-        while (keyboard.read_key() != "y"):
-            True
-        print ("-----------------------")
-        print ("   Key Input Enabled   ")
-        print ("-----------------------")
-    if keyboard.read_key() == "k":
+    if inputkey == "k":
         print ("-----------------------")
         print ("   Toggle Keepalive    ")
         print ("-----------------------")
@@ -382,9 +416,22 @@ def execute_code(connmodearg, port):
         else:
             keepalive=True
             print ("   Keepalive Enabled    ")
-    if keyboard.read_key() == "h":
+    if inputkey == "g":
+        print ("-----------------------")
+        print (" Toggle GPS Emulation  ")
+        print ("-----------------------")
+        if emulategps:
+            emulategps=False
+            print ("  GPS Emulation Disabled    ")
+        else:
+            activedevices.append(hex(176)) if hex(176) not in activedevices else activedevices
+            gpslat=float(input("Enter GPS Latitude in Decimal Format: "))
+            gpslon=float(input("Enter GPS Longitude in Decimal Format: "))
+            emulategps=True
+            print ("  GPS Emulation Enabled    ")
+    if inputkey == "h":
         printhelpmenu()
-    if keyboard.read_key() == "q":
+    if inputkey == "q":
         break
     time.sleep(.1)
   closeconn()
